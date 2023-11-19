abd3ff8f6ec350c9e393f1e8231884dd2022b345
platara <plat0033@algonquinlive.com>
2023-11-19 14:25:16 -0500

    add banner

---
+++ components/Feature.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <h2>Feature component</h2>
+    <div v-if="body" class="card">
+        <h2>{{ body[1].name }}</h2>
+        <img :src="body[1].image.filename" alt="">
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props:['body']
+    }
+</script>
+
+<style>
+    .card{
+        margin-left: 2rem;
+    }
+</style>
